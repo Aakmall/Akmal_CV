1145918ade83d8457e7823995d97f1a9c9c224e0
Aakmall <aknapu9784635@gmail.com>
2024-11-19 23:49:25 +0700

    add gambar

---
+++ src/components/home/HeaderSocials.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HeaderSocials = () => {
+  return (
+    <div>
+      <div className="home__socials">
+        <a href="" className="home__social-link" target="_blank"></a>
+      </div>
+    </div>
+  )
+}
+
+export default HeaderSocials;
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,12 +1,17 @@
-import React from 'react';
+import React from "react";
 import "./home.css";
+import me from "../../Assets/botak(2.jpg";
 
 const Home = () => {
   return (
-    <div>
-      <div>pppppppppppppppppppppppppppppppp</div>
-    </div>
-  )
-}
+    <section className="home container" id="home">
+      <div>
+        <img src={me} alt="" className="home__img" />
+        <h1 className="home__name">Akmal Rizki Anapu</h1>
+        <span className="home__education">Saya adalah programmer pemula</span>
+      </div>
+    </section>
+  );
+};
 
 export default Home;
