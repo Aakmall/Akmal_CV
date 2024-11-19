84d8429e6b9f22da7de555c6664b3effd5c2ccbf
Aakmall <aknapu9784635@gmail.com>
2024-11-19 21:26:13 +0700

    adding css 1

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import React from 'react';
-import "./sidebar.css";
 import Sidebar from './components/sidebar/Sidebar';
 import Home from './components/home/Home';
 import About from './components/about/About';
 import Services from './components/service/Services';
 import Resume from './components/resume/Resume';
 import Portofolio from './components/portofolio/Portofolio';
-
+import Contact from './components/contact/Contact';
+import Blog from './components/blog/Blog';
+import Testimonials from './components/testmonials/Testimonials';
 
 const App = () => {
   return (
     <div>
-      App
+      <Sidebar />
+      <main classname='main'>
+        <Home />
+        <About />
+        <Services />
+        <Resume />
+        <Portofolio />
+        <Testimonials />
+        <Blog />
+        <Contact />
+      </main>
     </div>
   )
 }
---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Sidebar.css";
+import 
+
+const Sidebar = () => {
+  return (
+    <div>
+      <asside className='aside'>
+        <a href="#home" className="nav__logo"></a>
+      </asside>
+    </div>
+  )
+}
+
+export default Sidebar;
